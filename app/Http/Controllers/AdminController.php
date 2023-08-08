dfd832e22e254457e35febc4b4df1afaa9f23b73
知念英弥 <s19013@std.it-college.ac.jp>
2023-08-08 13:02:37 +0900

    新規登録画面への遷移と登録処理

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,11 +5,34 @@ namespace App\Http\Controllers;
 use App\Models\Movie;
 use Illuminate\Http\Request;
 
+use App\Http\Requests\CreateMovieRequest;
+
+use App\Http\Repository\MovieRepository;
+
+use DB;
+
 class AdminController extends Controller
 {
+    private $movieRepository;
+
+    public function __construct()
+    {
+        $this->movieRepository  = new MovieRepository();
+    }
+
     public function index()
     {
         $movies = Movie::all();
         return view('admin/index', ['movies' => $movies]);
+    }
+
+    public function create() {
+        return view('admin/create');
+    }
+
+    public function store(CreateMovieRequest $request)
+    {
+        $this->movieRepository->store($request);
+        return redirect('admin/movies');
     }
 }
