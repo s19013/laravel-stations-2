bbacc75e66227a0c926091f8f20398ca4f58c945
知念英弥 <s19013@std.it-college.ac.jp>
2023-08-08 15:48:44 +0900

    少しみにく買ったので整理

--- resources/views/admin/movie/index.blade.php
+++ resources/views/admin/movie/index.blade.php
@@ -6,22 +6,32 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Practice</title>
 </head>
+<style>
+table td{
+    border-collapse: collapse;
+    border: 1px solid #000000;
+    padding: 5px;
+}
+</style>
 <body>
-    <tbody>
-        @foreach ($movies as $movie)
-        <tr><td>タイトル: {{ $movie->title }}</td></tr>
-        {{-- <tr><td><img src={{ $movie->image_url }} alt=""></td></tr> --}}
-        <tr><td>{{ $movie->image_url }}</td></tr>
-        <tr><td>公開年: {{ $movie->published_year }}</td></tr>
-        <tr>
-            @if ($movie->is_showing)
-                <td>上映中</td>
-            @else
-                <td>上映予定</td>
-            @endif
-        </tr>
-        <tr><td>{{ $movie->description }}</td></tr>
-        @endforeach
-    </tbody>
+    <table>
+        <tbody>
+            @foreach ($movies as $movie)
+            <tr>
+            <td>タイトル: {{ $movie->title }}</td>
+            {{-- <tr><td><img src={{ $movie->image_url }} alt=""></td></tr> --}}
+            <td>{{ $movie->image_url }}</td>
+            <td>公開年: {{ $movie->published_year }}</td>
+
+                @if ($movie->is_showing)
+                    <td>上映中</td>
+                @else
+                    <td>上映予定</td>
+                @endif
+            <td>{{ $movie->description }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 </body>
 </html>
