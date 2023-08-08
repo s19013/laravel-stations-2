0bd94201e9ecce9e05e0dd6c38ee83a3314fadf9
知念英弥 <s19013@std.it-college.ac.jp>
2023-08-08 17:04:49 +0900

    編集画面作成

---
+++ resources/views/admin/movie/edit.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Practice</title>
+</head>
+<body>
+    @if ($errors->any())
+        <ul >
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
+    <form action="/admin/movies/{{$movie->id}}/update" method="post">
+        <input type="hidden" name='id' value={{$movie->id}}>
+        @method('patch')
+        @csrf
+        <label >
+            タイトル
+            <input name='title' type="text" value="{{old('title',$movie->title)}}" required >
+        </label>
+
+        <label >
+            画像のURL
+            <input name='image_url' type="text" value="{{old('image_url',$movie->image_url)}}" required>
+        </label>
+
+        <label >
+            公開年
+            <input type="number" name="published_year" value={{ old('published_year',$movie->published_year) }} required>
+        </label>
+
+        <label >
+            公開中
+            <input name="is_showing" type="hidden" value="0" >
+            <input type="checkbox" name="is_showing" value="1"  @if ((int)old('is_showing',$movie->is_showing) == "1") checked @endif>
+        </label>
+
+        <label >
+            概要
+            <textarea name="description" id="" cols="30" rows="10" required>{{old('description',$movie->description)}}</textarea>
+        </label>
+
+        <input type="submit" value="送信">
+    </form>
+</body>
+
+</html>
