b0f33f752672ba464db8802648f1b900f67453b5
知念英弥 <s19013@std.it-college.ac.jp>
2023-08-09 19:58:37 +0900

    シーダーをジャンルに対応

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Movie;
+use App\Models\Genere;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,6 +19,7 @@ class DatabaseSeeder extends Seeder
         //     SheetTableSeeder::class,
         // ]);
 
+        Genere::factory(10)->create();
         Movie::factory(25)->create();
     }
 }
