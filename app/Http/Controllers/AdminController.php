6846dd4365916ebfae535666eb70d0a660c63bd5
知念英弥 <s19013@std.it-college.ac.jp>
2023-08-09 22:23:02 +0900

    ジャンルを登録する処理を追加した

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,21 +9,24 @@ use App\Http\Requests\CreateMovieRequest;
 use App\Http\Requests\UpdateMovieRequest;
 
 use App\Http\Repository\MovieRepository;
+use App\Http\Repository\GenereRepository;
 
 use DB;
 
 class AdminController extends Controller
 {
     private $movieRepository;
+    private $genereRepository;
 
     public function __construct()
     {
         $this->movieRepository  = new MovieRepository();
+        $this->genereRepository = new GenereRepository();
     }
 
     public function index()
     {
-        $movies = Movie::all();
+        $movies = Movie::with('genere')->get();
         return view('admin/movie/index', ['movies' => $movies]);
     }
 
@@ -33,19 +36,34 @@ class AdminController extends Controller
 
     public function store(CreateMovieRequest $request)
     {
-        // try_catch必要か?
-        $this->movieRepository->store($request);
+        if ($this->genereRepository->isExists($request->genere)) {
+            $this->movieRepository->store($request);
+            return redirect('admin/movies');
+        }
+
+        DB::transaction(function () use($request){
+            $request->genereId = $this->genereRepository->storeAndReturnId($request->genere);
+            $this->movieRepository->store($request);
+        });
         return redirect('admin/movies');
     }
 
     public function edit(Request $request) {
-        $movie = Movie::find((int) $request->id);
+        $movie = Movie::find((int)$request->id);
         return view('admin/movie/edit',['movie' => $movie]);
     }
 
     public function update(UpdateMovieRequest $request) {
         // try_catch必要か?
-        $this->movieRepository->update($request);
+        if ($this->genereRepository->isExists($request->genere)) {
+            $this->movieRepository->update($request);
+            return redirect('admin/movies');
+        }
+
+        DB::transaction(function () use($request){
+            $request->genereId = $this->genereRepository->storeAndReturnId($request->genere);
+            $this->movieRepository->update($request);
+        });
         return redirect('admin/movies');
     }
 
