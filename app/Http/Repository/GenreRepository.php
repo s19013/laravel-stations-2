339ff406ebbdf4eef82b8ed46ab2e8fd22f19f59
知念英弥 <s19013@std.it-college.ac.jp>
2023-08-09 23:16:18 +0900

    idを返すのと登録は別にした

--- app/Http/Repository/GenreRepository.php
+++ app/Http/Repository/GenreRepository.php
@@ -7,13 +7,17 @@ use Illuminate\Http\Request;
 
 class GenreRepository {
 
-    public function storeAndReturnId(string $name) {
-        $genre = Genre::create([
+    public function store(string $name) {
+        Genre::create([
             'name'    => $name,
         ]);
+    }
 
+    public function returnIdFromName(string $name){
+        $genre = Genre::where('name','=',$name)->first();
         return $genre->id;
     }
+
 
     public function isExists(string $name)  {
         return Genre::where('name','=',$name)->exists();
