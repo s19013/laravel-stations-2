3a9255f64bb5d791377fcc2f5497c821a5533658
知念英弥 <s19013@std.it-college.ac.jp>
2023-08-10 12:41:14 +0900

    リレーション設定

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -5,19 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Movie extends Model
+class schedule extends Model
 {
     use HasFactory;
 
     protected $guarded = ['id','created_at','updated_at'];
 
     // リレーション
-    public function genre()  {
-        return $this->belongsTo(Genre::class);
-    }
-
-    public function schedule()
+    public function movie()
     {
-        return $this->hasMany(Schedule::class);
+       return $this->belongsTo(Movie::class);
     }
 }
