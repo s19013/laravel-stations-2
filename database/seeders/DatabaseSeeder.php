852b93777a423bfad4f01bab706200abd2e85dfd
知念英弥 <s19013@std.it-college.ac.jp>
2023-08-10 16:30:04 +0900

    テストのためにコメントアウト

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,9 +19,9 @@ class DatabaseSeeder extends Seeder
         $this->call([
             SheetTableSeeder::class,
         ]);
-        Genre::factory(10)->create();
-        Movie::factory(25)->create();
-        Schedule::factory(10)->create();
+        // Genre::factory(10)->create();
+        // Movie::factory(25)->create();
+        // Schedule::factory(10)->create();
 
     }
 }
