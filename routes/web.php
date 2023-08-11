99e9a9d416d58f74616382d842899bccfddf814e
知念英弥 <s19013@std.it-college.ac.jp>
2023-08-11 19:55:47 +0900

    新しいルーティング

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PracticeController;
 use App\Http\Controllers\MovieController;
-use App\Http\Controllers\AdminController;
 use App\Http\Controllers\SheetController;
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AdminScheduleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,13 +28,6 @@ Route::get('/practice2', [PracticeController::class,'sample2']);
 Route::get('/practice3', [PracticeController::class,'sample3']);
 Route::get('/getPractice',  [PracticeController::class,'getPractice']);
 
-Route::prefix('/movies')->group(function () {
-    Route::get('/',  [MovieController::class,'index']);
-    Route::get('/{id}',  [MovieController::class,'specifics']);
-});
-
-Route::get('/sheets',[SheetController::class,'master']);
-
 Route::prefix('admin/movies')->group(function () {
     Route::get('/',  [AdminController::class,'index']);
     Route::get('/create',  [AdminController::class,'create']);
@@ -40,4 +35,23 @@ Route::prefix('admin/movies')->group(function () {
     Route::get('/{id}/edit',  [AdminController::class,'edit']);
     Route::patch('/{id}/update',  [AdminController::class,'update']);
     Route::delete('/{id}/destroy',  [AdminController::class,'destroy']);
+
+    Route::get('/{id}',  [AdminController::class,'specifics']);//なぜかこれが先に読み込まれたことによりcreateに以降できなかったため下に持ってきた
+
+    // スケジュール
+    Route::get('{id}/schedules/create',  [AdminScheduleController::class,'create']);
+    Route::post('{id}/schedules/store',  [AdminScheduleController::class,'store']);
 });
+
+Route::prefix('admin/schedules')->group(function (){
+    Route::get('/{id}/edit',[AdminScheduleController::class,'edit']);
+    Route::patch('/{id}/update',[AdminScheduleController::class,'update']);
+    Route::delete('/{id}/destroy',[AdminScheduleController::class,'destroy']);
+});
+
+Route::prefix('/movies')->group(function () {
+    Route::get('/',  [MovieController::class,'index']);
+    Route::get('/{id}',  [MovieController::class,'specifics']);
+});
+
+Route::get('/sheets',[SheetController::class,'master']);
