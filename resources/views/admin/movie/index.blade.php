591b03d261c41b8fa3d5bdd427951c66c98c120d
知念英弥 <s19013@std.it-college.ac.jp>
2023-08-11 19:57:38 +0900

    今までのやり方だと見えにくいので改良

--- resources/views/admin/movie/index.blade.php
+++ resources/views/admin/movie/index.blade.php
@@ -7,50 +7,110 @@
     <title>Practice</title>
 </head>
 <style>
-table td{
-    border-collapse: collapse;
-    border: 1px solid #000000;
-    padding: 5px;
-}
+    table td{
+        border-collapse: collapse;
+        border: 1px solid #000000;
+        padding: 5px;
+    }
 </style>
 <body>
-    <table>
-        <tbody>
+    <a href="/admin/movies/create">
+        <button type="button">新規作成</button>
+    </a>
+    <table class="movieTable">
+        <tr>
+            <td></td>
             @foreach ($movies as $movie)
-            <tr>
-            <td>
-                <a href="/admin/movies/{{$movie->id}}/edit">
-                    編集
-                </a>
-            </td>
-            <td>
-                <form action="/admin/movies/{{$movie->id}}/destroy" method="post" onsubmit="return finalCheck()">
-                    @csrf
-                    @method('DELETE')
-                    <input type="submit" value="削除">
-                </form>
-            </td>
-            <td>タイトル: {{ $movie->title }}</td>
-            <td>ジャンル: {{ $movie->genre->name }}</td>
-            {{-- <tr><td><img src={{ $movie->image_url }} alt=""></td></tr> --}}
-            <td>{{ $movie->image_url }}</td>
-            <td>公開年: {{ $movie->published_year }}</td>
-
-                @if ($movie->is_showing)
-                    <td>上映中</td>
-                @else
-                    <td>上映予定</td>
-                @endif
-            <td>{{ $movie->description }}</td>
-            </tr>
+                <td>
+                    <a href="/admin/movies/{{$movie->id}}/edit">
+                        <button type="button">編集</button>
+                    </a>
+                </td>
             @endforeach
-        </tbody>
+        </tr>
+        <tr>
+            <td></td>
+            @foreach ($movies as $movie)
+                <td>
+                    <a href="/admin/movies/{{$movie->id}}">
+                        <button type="button">詳細</button>
+                    </a>
+                </td>
+            @endforeach
+        </tr>
+        <tr>
+            <td></td>
+            @foreach ($movies as $movie)
+                <td>
+                    <form action="/admin/movies/{{$movie->id}}/destroy" method="post" onsubmit="return finalCheck()">
+                        @csrf
+                        @method('DELETE')
+                        <input type="submit" id='delete' value="削除">
+                    </form>
+                </td>
+            @endforeach
+        </tr>
+        <tr>
+            <td>ID</td>
+            @foreach ($movies as $movie)
+                <td>{{$movie->id}}</td>
+            @endforeach
+        </tr>
+        <tr>
+            <td>タイトル</td>
+            @foreach ($movies as $movie)
+                <td>{{$movie->title}}</td>
+            @endforeach
+        </tr>
+        <tr>
+            <td>画像</td>
+            @foreach ($movies as $movie)
+            <td>
+                <img width="100rem" src="{{$movie->image_url}}" alt="">
+            </td>
+            @endforeach
+        </tr>
+        <tr>
+            <td>公開年</td>
+            @foreach ($movies as $movie)
+                <td>{{$movie->published_year}}</td>
+            @endforeach
+        </tr>
+        <tr>
+            <td>上映状態</td>
+            @foreach ($movies as $movie)
+                @if ($movie->is_showing)
+                <td>上映中</td>
+                @endif
+                @if ($movie->is_showing == false)
+                <td>上映予定</td>
+                @endif
+            @endforeach
+        </tr>
+        <tr>
+            <td>概要</td>
+            @foreach ($movies as $movie)
+                <td>{{$movie->description}}</td>
+            @endforeach
+        </tr>
+        <tr>
+            <td>登録日時</td>
+            @foreach ($movies as $movie)
+                <td>{{$movie->created_at}}</td>
+            @endforeach
+        </tr>
+        <tr>
+            <td>更新日時 </td>
+            @foreach ($movies as $movie)
+                <td>{{$movie->updated_at}}</td>
+            @endforeach
+        </tr>
     </table>
+    </div>
+    <script>
+        function finalCheck() {
+            window.confirm("削除しますか?");
+        }
+    </script>
 </body>
-<script>
-    // 確認ダイアログ
-    function finalCheck() {
-        window.confirm("削除しますか?");
-    }
-</script>
 </html>
