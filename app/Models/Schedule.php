2e8990977cfb55bfc31a9cba437e917491191efd
知念英弥 <s19013@std.it-college.ac.jp>
2023-08-11 17:26:11 +0900

    考えメモ

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -10,7 +10,10 @@ class schedule extends Model
     use HasFactory;
 
     protected $guarded = ['id','created_at','updated_at'];
-    protected $dates = ['start_time','end_time'];//これでformatが使える
+    protected $dates = ['start_time','end_time'];
+    //これでformatが使える
+    // date(, strtotime())を使う方法もあるが､model使ってるんだからformat()を使おうと思う
+
 
     // リレーション
     public function movie()
