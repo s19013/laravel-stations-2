b42cdf492f68e18e9119c6975f787c07fe84d274
知念英弥 <s19013@std.it-college.ac.jp>
2023-08-12 20:23:53 +0900

    テストの時はコメントアウト

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,25 +21,22 @@ class DatabaseSeeder extends Seeder
         $this->call([
             SheetTableSeeder::class,
         ]);
-        // Genre::factory(10)->create();
-        $now = CarbonImmutable::now();
-        $movie = Movie::factory()->create();
-        Schedule::factory()->create([
-            'movie_id' => $movie->id,
-            'start_time' => $now->addHours(10),
-            'end_time'   => $now::now()->addHours(11)
-        ]);
-        Schedule::factory()->create([
-            'movie_id' => $movie->id,
-            'start_time' => $now::now()->addHours(3),
-            'end_time'   => $now::now()->addHours(4)
-        ]);
-        Schedule::factory()->create([
-            'movie_id' => $movie->id,
-            'start_time' => $now::now()->addHours(8),
-            'end_time'   => $now::now()->addHours(9)
-        ]);
-        // Schedule::factory(10)->create();
-
+        // $now = CarbonImmutable::now();
+        // $movie = Movie::factory()->create();
+        // Schedule::factory()->create([
+        //     'movie_id' => $movie->id,
+        //     'start_time' => $now->addHours(10),
+        //     'end_time'   => $now::now()->addHours(11)
+        // ]);
+        // Schedule::factory()->create([
+        //     'movie_id' => $movie->id,
+        //     'start_time' => $now::now()->addHours(3),
+        //     'end_time'   => $now::now()->addHours(4)
+        // ]);
+        // Schedule::factory()->create([
+        //     'movie_id' => $movie->id,
+        //     'start_time' => $now::now()->addHours(8),
+        //     'end_time'   => $now::now()->addHours(9)
+        // ]);
     }
 }
