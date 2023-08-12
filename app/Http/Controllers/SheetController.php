fcf00b98ce9c68f86098fd12804d39711a2ac997
知念英弥 <s19013@std.it-college.ac.jp>
2023-08-12 16:34:59 +0900

    シート予約画面への遷移

--- app/Http/Controllers/SheetController.php
+++ app/Http/Controllers/SheetController.php
@@ -7,15 +7,19 @@ use Illuminate\Http\Request;
 
 class SheetController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function master()
     {
         return view('sheet.master', [
             "sheets" => Sheet::all()
+        ]);
+    }
+
+    public function index(Request $request) {
+        return view('sheet.index', [
+            "sheets" => Sheet::all(),
+            "movieId" => $request->movieId,
+            "scheduleId" => $request->scheduleId,
+            "date" => $request->date
         ]);
     }
 }
