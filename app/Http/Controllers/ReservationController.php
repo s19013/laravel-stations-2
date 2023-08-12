9669d354b8e367c281caf01608b4183bd32cba97
知念英弥 <s19013@std.it-college.ac.jp>
2023-08-12 20:52:13 +0900

    条件を通過しなかったらエラーを返す

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -1,12 +1,13 @@
 <?php
 declare(strict_types=1);
 namespace App\Http\Controllers;
-
-use App\Practice;
+use App\Models\Reservation;
 use Illuminate\Http\Request;
 use App\Http\Requests\CreateReservationRequest;
 
 use App\Http\Repository\ReservationRepository;
+
+use Validator;
 
 class ReservationController extends Controller
 {
@@ -18,6 +19,11 @@ class ReservationController extends Controller
     }
 
     public function create(Request $request) {
+        // dd(Reservation::all());
+
+
+        if (is_null($request->date) || is_null($request->sheetId) ) { abort(400,); }
+
         return view('reservation', [
             'movieId' => $request->movieId,
             'scheduleId' => $request->scheduleId,
@@ -27,6 +33,11 @@ class ReservationController extends Controller
     }
 
     public function store(CreateReservationRequest $request)  {
+        // 重複があるか?
+        if ($this->reservationRepository->isExists($request)) {
+            return redirect("/movies/{$request->movieId}");
+        }
+
         $this->reservationRepository->store($request);
         return redirect('/movies');
     }
--- app/Http/Controllers/SheetController.php
+++ app/Http/Controllers/SheetController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Sheet;
 use Illuminate\Http\Request;
 
+use Validator;
+
 class SheetController extends Controller
 {
     public function master()
@@ -15,6 +17,11 @@ class SheetController extends Controller
     }
 
     public function index(Request $request) {
+        // dateがあるか確認
+        // validatorだと200でリダイレクトとするからテスト通らない
+
+        if (is_null($request->date)) {abort(400); }
+
         return view('sheet.index', [
             "sheets" => Sheet::all(),
             "movieId" => $request->movieId,
