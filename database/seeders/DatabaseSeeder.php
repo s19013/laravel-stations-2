d56912950554d8dba417b83f3cc6ae5146aa089e
知念英弥 <s19013@std.it-college.ac.jp>
2023-08-14 00:30:44 +0900

    色々追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Seeder;
 use App\Models\Movie;
 use App\Models\Genre;
 use App\Models\Schedule;
+use App\Models\Reservation;
 
 use Carbon\CarbonImmutable;
 
@@ -23,20 +24,34 @@ class DatabaseSeeder extends Seeder
         ]);
         // $now = CarbonImmutable::now();
         // $movie = Movie::factory()->create();
-        // Schedule::factory()->create([
+        // $oldSchedule = Schedule::factory()->create([
         //     'movie_id' => $movie->id,
-        //     'start_time' => $now->addHours(10),
-        //     'end_time'   => $now::now()->addHours(11)
+        //     'start_time' => $now->subHours(20),
+        //     'end_time'   => $now->subHours(21)
+        // ]);
+        // $newSchedule = Schedule::factory()->create([
+        //     'movie_id' => $movie->id,
+        //     'start_time' => new CarbonImmutable('2050-01-01 00:00:00'),
+        //     'end_time'   => new CarbonImmutable('2050-01-01 02:00:00')
         // ]);
         // Schedule::factory()->create([
         //     'movie_id' => $movie->id,
-        //     'start_time' => $now::now()->addHours(3),
-        //     'end_time'   => $now::now()->addHours(4)
+        //     'start_time' => $now->addHours(8),
+        //     'end_time'   => $now->addHours(9)
         // ]);
-        // Schedule::factory()->create([
-        //     'movie_id' => $movie->id,
-        //     'start_time' => $now::now()->addHours(8),
-        //     'end_time'   => $now::now()->addHours(9)
+        // Reservation::insert([
+        //     'date' => $newSchedule->start_time,
+        //     'schedule_id' => $newSchedule->id,
+        //     'sheet_id' => 1,
+        //     'email' => 'sample@exmaple.com',
+        //     'name' => 'サンプル太郎',
+        // ]);
+        // Reservation::insert([
+        //     'date' => $oldSchedule->start_time,
+        //     'schedule_id' => $oldSchedule->id,
+        //     'sheet_id' => 1,
+        //     'email' => 'sample@exmaple.com',
+        //     'name' => 'サンプル太郎',
         // ]);
     }
 }
