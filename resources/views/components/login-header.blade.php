354003c93453666637fc673b55dd082bb33c6488
知念英弥 <s19013@std.it-college.ac.jp>
2023-08-15 17:16:55 +0900

    ログインログアウトリンクをコンポーネント化した

---
+++ resources/views/components/login-header.blade.php
@@ -0,0 +1,20 @@
+<div>
+    @if (Route::has('login'))
+    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+        @auth
+        <form action="{{ route('logout') }}" method="post">
+            @csrf
+
+            <input type="submit" value="log out">
+        </form>
+            {{-- <a href="{{ route('logout') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">log out</a> --}}
+        @else
+            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+            @endif
+        @endauth
+    </div>
+    @endif
+</div>
